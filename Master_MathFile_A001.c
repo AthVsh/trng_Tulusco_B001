06ef0e4e74270df947e93a65e78450399432cb61
AthVsh <amruth.kv@hotmail.com>
2018-01-08 15:14:05 +0000

    Create Master_MathFile_A001.c

---
+++ Master_MathFile_A001.c
@@ -0,0 +1,23 @@
+
+////////// Master: Master_MathFile_A001.c
+
+
+////////// Function-A001 /////////////////////////
+long double func_A001_add(double a, double b) 
+{   
+    long double result;   
+    result = (long double) (a) +  (long double) (b);    
+    return result;  
+}  
+
+////////// Function-A002 /////////////////////////
+long double func_A002_sub(double a, double b) 
+{   
+   long double result;    
+   result = (long double) (a) -  (long double) (b); 
+   return result;  
+}
+
+//////////////////////////////////////////////////
+//||||||||||||||||||||||||||||||||||||||||||||||
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
